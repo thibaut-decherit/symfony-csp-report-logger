c105b83ced6287e8af9c92b7e25b6b11bb4e6bf1
Thibaut Decherit <tdecherit@entreloups.com>
2019-05-02 14:45:09 +0200

    homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
+class HomeController extends DefaultController
+{
+    /**
+     * Renders homepage.
+     *
+     * @Route(name="home", methods="GET")
+     * @return Response
+     */
+    public function home()
+    {
+        return $this->render('home.html.twig');
+    }
+}
